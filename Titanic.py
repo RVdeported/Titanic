c7f658fdc4fe33b57f8f30b8cb957bc590f56907
RVdeported <romanvetrin@gmail.com>
2021-05-30 20:13:03 +0300

    Title classification

--- Titanic.py
+++ Titanic.py
@@ -36,7 +36,62 @@ df_full = pd.DataFrame(pd.read_csv(DATASET_TRAIN_PATH))
 df_full.index = df_full["PassengerId"]
 df_full = df_full.drop(["PassengerId"], axis = 1)
 df = df_full.drop(["Survived"], axis = 1).copy()
-label = pd.DataFrame(df_full["Survived"].copy())
+labels = pd.DataFrame(df_full["Survived"].copy())
+
+
+class DataPipeLine:
+    def __init__(self):
+        self.titles = {
+                'Mr' : 'Ordinary',
+                'Mrs' : 'Ordinary',
+                'Miss' : 'Ordinary',
+                'Master' : 'High',
+                'Rev' : 'Ordinary',
+                'Dr' : 'High',
+                'Mme' : 'Ordinary',
+                'Ms' : 'Ordinary',
+                'Major' : 'High',
+                'Lady' : 'High',
+                'Sir' : 'High',
+                'Mlle' : 'Ordinary',
+                'Col' : 'High',
+                'Capt' : 'Ordinary',
+                'the Countess' : 'High',
+                'Jonkheer' : 'High',
+                'Don' : 'High'
+                }
+        self.default_title = "Ordinary"
+        
+    
+    def fit(self, df_, label_):
+        #loc_df = df_.copy()
+        pass
+
+    def transform(self, df_, label_):
+        loc_df = df_.copy()
+        
+        # title classification
+        loc_df = self.addTitle(loc_df)
+        loc_df["Title_class"] = loc_df["Title"].apply(self.title_class) == "High"
+        
+        
+        return loc_df
+    
+    def addTitle(self, df_):
+        res = df_.copy()
+        res['Title'] = (df_["Name"].apply(lambda x: x[
+                x.find(", ") + 2:
+                x.find(".")]
+            ))
+        return res
+    
+    def title_class(self, x):
+        try:
+            return self.titles[x]
+        except KeyError:
+            return self.default_title
+
+
 
 # passangers from 1st class were more likely to survive
 plt.hist(df["Pclass"][label[label["Survived"] == 1].index], 
@@ -45,5 +100,13 @@ plt.hist(df["Pclass"][label[label["Survived"] == 0].index],
          density = 0.5)
 plt.show()
 
-df["Pclass"]
-label[label["Survived"] == 0].index
+df["Title"].value_counts()
+
+
+
+pipe = DataPipeLine()
+df_prepared = pipe.transform(df, labels)
+    
+
+
+
