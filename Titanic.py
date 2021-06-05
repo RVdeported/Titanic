74e635d4a9b7c4577d35d6a6772429963ee18c1d
RVdeported <romanvetrin@gmail.com>
2021-06-05 14:40:49 +0300

    Final predictions

--- Titanic.py
+++ Titanic.py
@@ -13,12 +13,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle
 
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder
 
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.svm import SVC
 
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, BaggingClassifier
 from sklearn.linear_model import LinearRegression as LR
+
+from sklearn.metrics import confusion_matrix, precision_recall_curve,accuracy_score, precision_score, f1_score, recall_score
 
 DATASET_TRAIN_PATH = "./input/train.csv"
 DATASET_TEST_PATH = "./input/test.csv"
@@ -27,7 +30,7 @@ PREPARED_TEST_DATASET_PATH = "./output/test_prepared.csv"
 OUTPUT_PATH = "./output/test_predicted.csv"
 MODEL_PATH = "./output/model.pickle"
 
-RANDOM_VAL = 228
+RANDOM_VAL = 228+14+25
 
 
 
@@ -71,6 +74,7 @@ class DataPipeLine:
         self.avg = df_[self.numeric_features].mean()
         
         self.sc = StandardScaler(with_mean = False)
+        #self.sc = MinMaxScaler()
         self.encoder = OneHotEncoder()
         
         loc_df = self.transform(df_, False)
@@ -103,8 +107,8 @@ class DataPipeLine:
         loc_df = loc_df.drop(["Sex"], axis = 1)
         
         # age cat
-        loc_df["Underage"] = loc_df["Age"] <= 14
-        loc_df["Middleage"] = ((loc_df["Age"] > 14) & 
+        loc_df["Underage"] = loc_df["Age"] <= 18
+        loc_df["Middleage"] = ((loc_df["Age"] > 18) & 
                                (loc_df["Age"] < 45))
         loc_df["Elderly"] = loc_df["Age"] >= 45
         
@@ -112,8 +116,6 @@ class DataPipeLine:
         loc_df = loc_df.drop(["Name", "Cabin", "Ticket"], axis = 1)
   
         # other cat
-
-        # dropping other
         loc_df["Embarked_S"] = loc_df["Embarked"] == "S"
         loc_df["Embarked_C"] = loc_df["Embarked"] == "C"
         loc_df["Embarked_Q"] = loc_df["Embarked"] == "Q"
@@ -122,13 +124,17 @@ class DataPipeLine:
         loc_df["Level_C"] = loc_df["Level"] == "C"
         loc_df["Level_D"] = loc_df["Level"] == "D"
         loc_df["Level_E"] = loc_df["Level"] == "E"
+        loc_df["High_deck"] = (loc_df["Level"] == "A") | (loc_df["Level"] == "B")
+        loc_df["Have_sibs"] = loc_df["SibSp"] > 0
+        loc_df["Have_Parch"] = loc_df["Parch"] > 0
+        loc_df["Female_with_kids"] = (loc_df["SibSp"] > 0) & (loc_df["Is_female"])
+        
         
         
         loc_df = loc_df.drop(["Level"], axis = 1)
         loc_df = loc_df.drop(["Embarked"], axis = 1)
          
         if (scale):
-            print("Scaling...")
             loc_df = pd.DataFrame(self.sc.transform(loc_df), columns = loc_df.columns)
         
         
@@ -165,10 +171,87 @@ plt.hist(df["Pclass"][labels[labels["Survived"] == 0].index],
 plt.show()
 
 
-
-
 pipe = DataPipeLine()
 pipe.fit(df, labels)
 df_prepared = pipe.transform(df)
 
+# Data split and preparation
+X_train, X_val, y_train, y_val = train_test_split(df, labels, test_size = 0.2, 
+                                                    random_state = RANDOM_VAL, 
+                                                    stratify = labels)
+pipe = DataPipeLine()
+pipe.fit(X_train, y_train)
+X_train_prepared = pipe.transform(X_train)
+X_val_prepared = pipe.transform(X_val)
 
+
+# model
+model = GradientBoostingClassifier(
+    max_depth = 2,
+    loss = 'deviance',
+    n_estimators = 120,
+    random_state = RANDOM_VAL
+)
+model_base = SVC(probability = True,
+            kernel = 'rbf',
+            degree = 3,
+            gamma = 'scale')
+
+model = BaggingClassifier(model_base, 
+                          n_estimators = 6,
+                          random_state = RANDOM_VAL)
+# Grid search
+"""
+params = {
+    'n_estimators' : [3, 4, 5, 6]
+    }
+search = GridSearchCV(model, params)
+search.fit(X_train_prepared, y_train)
+print(search.best_params_)
+"""
+
+model.fit(X_train_prepared, y_train)
+
+threshold = 0.46
+
+y_train_proba = model.predict_proba(X_train_prepared)
+y_train_pred = [n[1] > threshold for n in y_train_proba]
+
+y_val_proba = model.predict_proba(X_val_prepared)
+y_val_pred = [n[1] > threshold for n in y_val_proba]
+
+
+cm_train = confusion_matrix(y_train, y_train_pred)
+
+print("train f_score: " + str(f1_score(y_train, y_train_pred)))
+print("train precision: " + str(precision_score(y_train, y_train_pred)))
+print("train recall: " + str(recall_score(y_train, y_train_pred)))
+print("train accuracy: " + str(accuracy_score(y_train, y_train_pred)))
+
+accuracy_score
+cm_val = confusion_matrix(y_val, y_val_pred)
+print("\nval f_score: " + str(f1_score(y_val, y_val_pred)))
+print("val precision: " + str(precision_score(y_val, y_val_pred)))
+print("val recall: " + str(recall_score(y_val, y_val_pred)))
+print("val accuracy: " + str(accuracy_score(y_val, y_val_pred)))
+
+
+
+print(cm_val)
+
+precision, recall, thresholds = precision_recall_curve(y_train.values,y_train_proba[:,1])
+plt.plot(recall, precision)
+plt.xlabel("Recall")
+plt.ylabel("Precision")
+plt.show()
+
+X_test = pd.DataFrame(pd.read_csv(DATASET_TEST_PATH))
+X_test.index = X_test["PassengerId"]
+X_test = X_test.drop(["PassengerId"], axis = 1)
+
+X_test_prepared = pipe.transform(X_test)
+
+y_test_proba = model.predict_proba(X_test_prepared)
+y_test_pred = [int(n[1] > threshold) for n in y_test_proba]
+y_test_pred = pd.DataFrame(y_test_pred, index = X_test.index, columns = ["Survived"])
+y_test_pred.to_csv(OUTPUT_PATH)
